ad7d9619d46f142c7854cf88312a295a60e3aecb
IrinaTab <irina.mariya.anatoliy@gmail.com>
2025-04-15 14:36:21 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+const burgerBtn = document.querySelector('.burger__btn');
+const burgerMenu = document.querySelector('.burger__menu');
+
+burgerBtn.addEventListener('click', function() {
+    burgerMenu.classList.toggle('show');
+});
+
+
+  
